c8bebe900075a9c8b3b1fd36438dc48a59cae4bc
Николай <fogumen@gmail.com>
2022-02-22 18:42:57 +0200

    tesseract

--- tesseract_parsing.py
+++ tesseract_parsing.py
@@ -1,8 +1,29 @@
+
 import pytesseract
 import cv2
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-img = cv2.imread('data/1.jpg')
+hodoriv = [cv2.imread('data/hodoriv/{}.jpg'.format(i)) for i in range(1, 6)]
+zbarazh = [cv2.imread('data/zbarazh/{}.jpg'.format(i)) for i in range(1, 8)]
+strymilo_kamenets = [cv2.imread('data/strymilo-kamenets/{}.jpg'.format(i)) for i in range(1, 6)]
 
-text = pytesseract.image_to_string(img, lang='ukr+eng')
-with open('1.txt', 'w', encoding='utf-8') as file:
+text = ''
+for img in hodoriv:
+    text += pytesseract.image_to_string(img, lang='ukr+eng')
+    text += '\n'
+text = text.replace('ті?', 'm2')
+text = text.replace('тп?', 'm2')
+text = text.replace('m?', 'm2')
+with open('text/hodoriv.txt', 'w', encoding='utf-8') as file:
+    file.write(text)
+text = ''
+for img in zbarazh:
+    text += pytesseract.image_to_string(img, lang='ukr+eng')
+    text += '\n'
+with open('text/zbarazh.txt', 'w', encoding='utf-8') as file:
+    file.write(text)
+text = ''
+for img in strymilo_kamenets:
+    text += pytesseract.image_to_string(img, lang='ukr+eng')
+    text += '\n'
+with open('text/stymilo-kamenets.txt', 'w', encoding='utf-8') as file:
     file.write(text)
