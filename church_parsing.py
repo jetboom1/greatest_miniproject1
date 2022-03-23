a42b9fb92511a9193a036531e6103e113b244276
YuriiKhr <yurii.kharabara@ucu.edu.ua>
2022-03-23 14:24:04 +0200

    churches normal parser added

---
+++ church_parsing.py
@@ -0,0 +1,63 @@
+from txt_parser import read_file
+from pprint import pprint
+import json
+def church_parser(text):
+    churches={"церкви":[]}
+    for row in text:
+        if ' ц.' in row: 
+            words=row.split(',')
+            try:
+                name=words[1][4:]
+            except IndexError:
+                name=''
+            try:
+                typpe=(words[2].split('.')[0]+'.')[1:]
+            except IndexError:
+                typpe=''
+            for i in range(10):
+                if str(i) in row:
+                    try:
+                        year=words[2].split('.')[1]
+                    except IndexError:
+                        year=''
+                else: year=''
+            church={
+                "назва": name,
+                "тип": typpe,
+                "рік": year[1:]
+            }
+            for i in range(10):
+                if str(i) in row[-20:]:
+                    try:
+                        key_for_year_2=words[3][1:4]+'.'
+                        year_2=words[3][6:]
+                        church[key_for_year_2]=year_2
+                    except IndexError:
+                        pass
+                else: year=''
+            
+            if "»" in row:
+                index_dn_start=row.rfind('»')+1
+                index_dn_end=row.rfind('«')
+                church["Дн."]=row[index_dn_start:index_dn_end]
+
+            if "будується" or 'мурується' or 'недокінчена' in row:
+                church['тип']=church['тип'].replace('\n', ' ')
+            if 'дер.' in row:
+                church['тип']='дер.'
+            church_final={}
+            for key in church:
+                if church[key]!='':
+                    church_final[key]=church[key]
+            churches['церкви'].append(church_final)
+        pprint(churches)
+    return churches
+
+if __name__=='__main__':
+    text=read_file('text/stymilo-kamenets.txt')
+    rows=text.split('\n\n')
+    print(rows)
+    churhes=church_parser(rows)
+    pprint(churhes)
+    with open("jsons/strymilo-kamenets_churhes.json", "w", encoding="utf-8") as file:
+        json.dump(churhes, file, indent=4, ensure_ascii=False)
